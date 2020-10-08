456da3ec8a34dfc2bf3410fe93db00e22106b3db
tabbycat-dev <tannguyen.tabby@gmail.com>
2020-10-09 02:31:28 +1100

    fix path error 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
@@ -21,20 +22,6 @@ module.exports = {
       },
     },
     "gatsby-plugin-sharp",
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          "gatsby-remark-relative-images",
-          {
-            resolve: "gatsby-remark-images",
-            options: {
-              maxWidth: 750,
-              linkImagesToOriginal: false,
-            },
-          },
-        ],
-      },
-    },
+    
   ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -8,20 +8,6 @@ const path = require('path')
 
 //Goal 3: generate new page for each post (using 'createPages')
 
-// module.exports.onCreateNode = ({ node, actions} )=> { 
-//     const { createNodeField } = actions
-//     if (node.internal.type === 'MarkdownRemark'){ 
-//         const slug = path.basename(node.fileAbsolutePath, '.md')
-//         console.log('@@@@@@@', slug)
-
-//         createNodeField({
-//             node,
-//             name: 'slug',
-//             value : slug     
-//         })
-
-//     }
-// }
 module.exports.createPages = async ({ graphql, actions }) => { 
     const { createPage } = actions
     const blogTemplate = path.resolve('./src/templates/blog.js')
